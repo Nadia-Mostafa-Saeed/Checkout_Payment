9f516ceb948354b657a6f3ce20f5e720c45731e9
nadiamostafa <nadiamostafasaeed@gmail.com>
2025-02-03 23:19:32 +0200

    create checkout repo impl

---
+++ lib/Features/checkout/data/repos/checkout_repo.dart
@@ -0,0 +1,8 @@
+import 'package:checkout/Features/checkout/data/models/payment_intent_input_model.dart';
+import 'package:checkout/errors/failures.dart';
+import 'package:dartz/dartz.dart';
+
+abstract class CheckoutRepo {
+  Future<Either<Failure, void>> makePayment(
+      {required PaymentIntentInputModel paymentIntentInputModel});
+}
---
+++ lib/Features/checkout/data/repos/checkout_repo_impl.dart
@@ -0,0 +1,21 @@
+import 'package:checkout/Features/checkout/data/models/payment_intent_input_model.dart';
+import 'package:checkout/Features/checkout/data/repos/checkout_repo.dart';
+import 'package:checkout/core/utils/stripe_service.dart';
+import 'package:checkout/errors/failures.dart';
+import 'package:dartz/dartz.dart';
+
+class CheckoutRepoImpl extends CheckoutRepo {
+  final StripeService stripeService = StripeService();
+  @override
+  Future<Either<Failure, void>> makePayment(
+      {required PaymentIntentInputModel paymentIntentInputModel}) async {
+    try {
+      await stripeService.makePayment(
+          paymentIntentInputModel: paymentIntentInputModel);
+
+      return right(null);
+    } catch (e) {
+      return left(ServerFailure(errMessage: e.toString()));
+    }
+  }
+}
