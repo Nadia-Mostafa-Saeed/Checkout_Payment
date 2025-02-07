5016d4c05b6a41eb7adca5c7ede268a2eb0987e8
nadiamostafa <nadiamostafasaeed@gmail.com>
2025-02-07 14:57:11 +0200

    create amount & item list model

--- lib/Features/checkout/presentation/widgets/custom_button_bloc_consumer.dart
+++ lib/Features/checkout/presentation/widgets/custom_button_bloc_consumer.dart
@@ -1,9 +1,11 @@
-import 'package:checkout/Features/checkout/data/models/payment_intent_input_model.dart';
+import 'dart:developer';
+
 import 'package:checkout/Features/checkout/presentation/manager/cubit/payment_cubit.dart';
 import 'package:checkout/Features/checkout/presentation/views/thank_you_view.dart';
 import 'package:checkout/core/utils/widgets/custom_button.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_paypal_payment/flutter_paypal_payment.dart';
 
 class CustomButtonBlockConsumer extends StatelessWidget {
   const CustomButtonBlockConsumer({
@@ -15,8 +17,7 @@ class CustomButtonBlockConsumer extends StatelessWidget {
     return BlocConsumer<PaymentCubit, PaymentState>(
       listener: (context, state) {
         if (state is PaymentSuccess) {
-          Navigator.of(context)
-              .pushReplacement(MaterialPageRoute(builder: (context) {
+          Navigator.of(context).push(MaterialPageRoute(builder: (context) {
             return const ThankYouView();
           }));
         }
@@ -30,14 +31,64 @@ class CustomButtonBlockConsumer extends StatelessWidget {
       builder: (context, state) {
         return CustomButton(
             onTap: () {
-              PaymentIntentInputModel paymentIntentInputModel =
+              Navigator.of(context).push(MaterialPageRoute(
+                builder: (BuildContext context) => PaypalCheckoutView(
+                  sandboxMode: true, // testing mode
+                  clientId: "YOUR CLIENT ID",
+                  secretKey: "YOUR SECRET KEY",
+                  transactions: const [
+                    {
+                      "amount": {
+                        "total": '100',
+                        "currency": "USD",
+                        "details": {
+                          "subtotal": '100',
+                          "shipping": '0',
+                          "shipping_discount": 0
+                        }
+                      },
+                      "description": "The payment transaction description.",
+                      "item_list": {
+                        "items": [
+                          {
+                            "name": "Apple",
+                            "quantity": 4,
+                            "price": '10',
+                            "currency": "USD"
+                          },
+                          {
+                            "name": "Pineapple",
+                            "quantity": 5,
+                            "price": '12',
+                            "currency": "USD"
+                          }
+                        ],
+                      },
+                    },
+                  ],
+                  note: "Contact us for any questions on your order.",
+                  onSuccess: (Map params) async {
+                    log("onSuccess: $params");
+                    Navigator.pop(context);
+                  },
+                  onError: (error) {
+                    log("onError: $error");
+                    Navigator.pop(context);
+                  },
+                  onCancel: () {
+                    print('cancelled:');
+                    Navigator.pop(context);
+                  },
+                ),
+              ));
+              /*PaymentIntentInputModel paymentIntentInputModel =
                   PaymentIntentInputModel(
                 amount: '100',
                 currency: 'USD',
                 customerId: 'cus_Rj3V20o35sFi1x',
               );
               BlocProvider.of<PaymentCubit>(context).makePayment(
-                  paymentIntentInputModel: paymentIntentInputModel);
+                  paymentIntentInputModel: paymentIntentInputModel);*/
             },
             isLoading: state is PaymentLoading ? true : false,
             title: 'Continue');
