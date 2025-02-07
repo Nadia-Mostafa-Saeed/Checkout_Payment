d33d3f12c793c8c4da54615b43cf21c9dbae0fe0
nadiamostafa <nadiamostafasaeed@gmail.com>
2025-02-07 15:13:06 +0200

    create transaction model

--- lib/Features/checkout/presentation/widgets/custom_button_bloc_consumer.dart
+++ lib/Features/checkout/presentation/widgets/custom_button_bloc_consumer.dart
@@ -1,5 +1,9 @@
 import 'dart:developer';
 
+import 'package:checkout/Features/checkout/data/models/amount_model/amount_model.dart';
+import 'package:checkout/Features/checkout/data/models/amount_model/details.dart';
+import 'package:checkout/Features/checkout/data/models/item_list_model/item.dart';
+import 'package:checkout/Features/checkout/data/models/item_list_model/item_list_model.dart';
 import 'package:checkout/Features/checkout/presentation/manager/cubit/payment_cubit.dart';
 import 'package:checkout/Features/checkout/presentation/views/thank_you_view.dart';
 import 'package:checkout/core/utils/widgets/custom_button.dart';
@@ -31,39 +35,44 @@ class CustomButtonBlockConsumer extends StatelessWidget {
       builder: (context, state) {
         return CustomButton(
             onTap: () {
+              var amountModel = AmountModel(
+                currency: "USD",
+                details: Details(
+                  shipping: "0",
+                  shippingDiscount: 0,
+                  subtotal: "100",
+                ),
+                total: "100",
+              );
+
+              List<Item> items = [
+                Item(
+                  currency: "USD",
+                  name: 'Apple',
+                  price: "4",
+                  quantity: 10,
+                ),
+                Item(
+                  currency: "USD",
+                  name: 'Apple',
+                  price: "5",
+                  quantity: 12,
+                ),
+              ];
+
+              var itemList = ItemListModel(
+                items: items,
+              );
               Navigator.of(context).push(MaterialPageRoute(
                 builder: (BuildContext context) => PaypalCheckoutView(
                   sandboxMode: true, // testing mode
                   clientId: "YOUR CLIENT ID",
                   secretKey: "YOUR SECRET KEY",
-                  transactions: const [
+                  transactions: [
                     {
-                      "amount": {
-                        "total": '100',
-                        "currency": "USD",
-                        "details": {
-                          "subtotal": '100',
-                          "shipping": '0',
-                          "shipping_discount": 0
-                        }
-                      },
+                      "amount": amountModel.toJson(),
                       "description": "The payment transaction description.",
-                      "item_list": {
-                        "items": [
-                          {
-                            "name": "Apple",
-                            "quantity": 4,
-                            "price": '10',
-                            "currency": "USD"
-                          },
-                          {
-                            "name": "Pineapple",
-                            "quantity": 5,
-                            "price": '12',
-                            "currency": "USD"
-                          }
-                        ],
-                      },
+                      "item_list": itemList.toJson(),
                     },
                   ],
                   note: "Contact us for any questions on your order.",
